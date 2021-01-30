6b98325a2abbe88e01ef0035999ff88652b88e02
vast-coding <vast.coding@gmail.com>
2021-01-30 18:45:45 +1100

    feat(mongodb): add User entity

---
+++ api/entity/User.ts
@@ -0,0 +1,19 @@
+import { Field, ObjectType } from 'type-graphql'
+import { prop as Property, getModelForClass } from '@typegoose/typegoose'
+
+import { ObjectId } from 'mongodb'
+
+@ObjectType()
+export class User {
+  @Field()
+  readonly _id: ObjectId
+
+  @Field()
+  @Property({ required: true })
+  email: string
+
+  @Property({ required: true })
+  password: string
+}
+
+export const UserModel = getModelForClass(User)
